b110aac0efc4b26b97a19b7209a3e5618041d92c
unknown <maqidong@maqidong-PC.(none)>
2017-01-02 22:04:49 +0800

    [NEW] add health-dao

---
+++ health-dao/src/main/java/com/personal/health/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.personal.health.dao;
+
+import com.personal.health.entity.User;
+
+public interface UserDao {
+
+	public User login(User user);
+}
---
+++ health-dao/src/main/java/com/personal/health/entity/User.java
@@ -0,0 +1,49 @@
+package com.personal.health.entity;
+
+
+public class User{
+
+	private Integer id;       //编号
+	private String username;  //用户名
+	private String password;  //密码
+	
+	
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
