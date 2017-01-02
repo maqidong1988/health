7624099a61e2befffb439ea6a5370a31869593f6
unknown <maqidong@maqidong-PC.(none)>
2017-01-02 22:10:44 +0800

    [NEW] add health-log

---
+++ health-log/src/main/java/com/personal/health/log/Logger.java
@@ -0,0 +1,14 @@
+package com.personal.health.log;
+
+import java.util.Date;
+
+/**
+ * Hello world!
+ *
+ */
+public class Logger 
+{
+    public static void main(String[] str) {
+        System.out.println(new Date()+"====="+ str);
+    }
+}
