235cb8f9c73c5fa47da56f42841c782f999f3be3
maqidong1988 <326717534@qq.com>
2017-01-07 13:37:56 +0800

    新增根据用户名查询用户接口,根据用户名查询角色接口,根据用户名查询权限接口

--- health-dao/src/main/java/com/personal/health/dao/UserDao.java
+++ health-dao/src/main/java/com/personal/health/dao/UserDao.java
@@ -1,8 +1,31 @@
 package com.personal.health.dao;
 
+import java.util.Set;
+
 import com.personal.health.entity.User;
 
 public interface UserDao {
 
 	public User login(User user);
+	
+	/**
+	 * 通过用户名查询用户
+	 * @param userName
+	 * @return
+	 */
+	public User getByUserName(String userName);
+	
+	/**
+	 * 通过用户名查询角色信息
+	 * @param userName
+	 * @return
+	 */
+	public Set<String> getRoles(String userName);
+	
+	/**
+	 * 通过用户名查询权限信息
+	 * @param userName
+	 * @return
+	 */
+	public Set<String> getPermissions(String userName);
 }
