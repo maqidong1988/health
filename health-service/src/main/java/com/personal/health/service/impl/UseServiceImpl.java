63c733aab505a5d62b3da5659360419dc2b978cb
maqidong1988 <326717534@qq.com>
2017-01-10 10:17:31 +0800

    新增userService了几个方法

--- health-service/src/main/java/com/personal/health/service/impl/UseServiceImpl.java
+++ health-service/src/main/java/com/personal/health/service/impl/UseServiceImpl.java
@@ -1,5 +1,8 @@
 package com.personal.health.service.impl;
 
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +15,26 @@ public class UseServiceImpl implements UserService{
 
 	@Autowired
 	private UserDao userDao;
+	
 	public User login(User user) {
 		return userDao.login(user);
+	}
+	
+	public List<User> getByUserName(String userName) {
+		return userDao.getByUserName(userName);
+	}
+	
+	public Set<String> getRoles(String userName) {
+		return userDao.getRoles(userName);
+	}
+	
+	public Set<String> getPermissions(String userName) {
+		return userDao.getPermissions(userName);
+	}
+
+	public User getByUserId(Integer userId) {
+		// TODO Auto-generated method stub
+		return userDao.getByUserId(userId);
 	}
 
 }
